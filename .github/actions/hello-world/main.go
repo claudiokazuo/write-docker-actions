affec1d8905fc81d443503fae98c74158e322192
Claudio Hirakawa <claudio.hirakawa@inteegra.com.br>
2021-04-12 10:27:14 -0300

    Extend Hello World's source code to accept the input parameters

--- .github/actions/hello-world/main.go
+++ .github/actions/hello-world/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	firstGreeting := os.Getenv("INPUT_FIRSTGREETING")
+	secondGreeting := os.Getenv("INPUT_SECONDGREETING")
+	thirdGreeting := os.Getenv("INPUT_THIRDGREETING")
+
+
+	fmt.Println("Hello" + firstGreeting)
+	fmt.Println("Hello" + secondGreeting)
+
+	if thirdGreeting != "" {
+		fmt.Println("Hello " + thirdGreeting)
+	}
+
 	fmt.Println("Hello Docker Actions")
 }
